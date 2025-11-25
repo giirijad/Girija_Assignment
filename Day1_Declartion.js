75aa5dcf71bd6579243da2983dbfa16cb2325cb0
giirijad <giirijad@gmail.com>
2025-11-25 09:04:02 -0500

    Add files via upload

---
+++ Day1_Declartion.js
@@ -0,0 +1,24 @@
+let firstName = "John";
+console.log( firstName); 
+console.log(typeof firstName);
+
+let companyName = "OpenAI";
+console.log( companyName); 
+console.log(typeof companyName);
+
+let mobileNumber = 1234567890;
+console.log( mobileNumber); 
+console.log(typeof mobileNumber);
+
+let isAutomation = true;
+console.log( isAutomation);
+console.log(typeof isAutomation);
+
+let hasPlaywright;
+console.log( hasPlaywright);
+console.log(typeof hasPlaywright);
+
+let address = null;
+console.log( address);
+console.log(typeof address);
+
