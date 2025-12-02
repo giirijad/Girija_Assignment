d08787ad75804c988582627e9319749d1930e08d
giirijad <giirijad@gmail.com>
2025-12-01 21:18:32 -0500

    Create W2_Day1_Array.js

---
+++ W2_Day1_Array.js
@@ -0,0 +1,16 @@
+function funoccurrences(input)
+{
+const nums = [2,4,5,2,1,2];
+let j=0
+for (k=0;k<=nums.length;k++)
+{
+    
+if (nums[k] == input)
+{
+    j++;
+}
+}
+return j;
+}
+
+console.log('No of time input appeared is  ' , funoccurrences(3));
